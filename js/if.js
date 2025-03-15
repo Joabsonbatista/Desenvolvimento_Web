c4c85cc5322e0c4297cc5357961e42f66107f0ab
Joabson Silva <batistasilvajoabson36@gmail.com>
2025-03-15 10:37:24 -0300

    primeiro comite

---
+++ js/if.js
@@ -0,0 +1,11 @@
+const numero = 7;
+
+if (numero >= 0 && numero <= 5) {
+  console.log("O numero esta entre 0 e 5");
+} else if (numero >= 6 && numero <= 8) {
+  console.log("numero esta  entre 6 e 8");
+} else if (numero >= 9 && numero <= 11) {
+  console.log("Numero esta entre 9 e 11");
+} else {
+  console.log("numero nao esta entre 0 e 11");
+}
